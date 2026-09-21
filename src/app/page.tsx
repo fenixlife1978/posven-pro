d767c2eba88e813f17408f261951bf71ec245e7c
fenixlife1978 <generaextra@gmail.com>
2026-09-21 18:54:09 -0400

    feat: pass active terminal to supplier payments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -354,7 +354,7 @@ export default function LicoreriaPOS() {
       case 'proveedores': return <SuppliersModule state={state} updateState={updateState} />;
       case 'contabilidad': return <AccountingModule state={state} updateState={updateState} />;
       case 'cxc': return <CxCModule state={state} updateState={updateState} />;
-      case 'cxp': return <CxPModule state={state} updateState={updateState} />;
+      case 'cxp': return <CxPModule state={state} updateState={updateState} terminalId={state.terminales.find(t => t.usuarioId === user?.uid)?.id} />;
       case 'reportes': return <ReportsModule state={state} />;
       case 'config': return <ConfigModule state={state} updateState={updateState} />;
       case 'usuarios': return <UsersModule />;
